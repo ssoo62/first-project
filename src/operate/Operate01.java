24d38bab4f752139822a17587181183df6f5b3f7
sujin mun <moon_62@naver.com>
2022-03-18 18:18:42 +0900

    first commit

--- src/operate/Operate01.java
+++ src/operate/Operate01.java
@@ -7,10 +7,10 @@ public class Operate01 {
 		boolean power = false;
 		System.out.println(power);
 		
-		power = !power;
+		power = !power; // false 값을 !를 이용해 뒤집는다
 		System.out.println(power);
 		
-		power = !power;
+		power = !power; // 뒤집었던 값(참)을 또 뒤집는다
 		System.out.println(power);
 		
 
