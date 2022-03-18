5a91473110626f875529204a551a64073a984f4f
sujin mun <moon_62@naver.com>
2022-03-18 18:03:14 +0900

    fisrt commit

---
+++ src/operate/Operate01.java
@@ -0,0 +1,19 @@
+package operate;
+
+public class Operate01 {
+
+	public static void main(String[] args) {
+		// boolean 이용하기
+		boolean power = false;
+		System.out.println(power);
+		
+		power = !power;
+		System.out.println(power);
+		
+		power = !power;
+		System.out.println(power);
+		
+
+	}
+
+}
