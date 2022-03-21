ae50931f9364e553370f42ae29544a5efdfd6866
sujin mun <moon_62@naver.com>
2022-03-21 22:49:02 +0900

    2

---
+++ src/operate/Operate02.java
@@ -0,0 +1,16 @@
+package operate;
+
+public class Operate02 {
+
+	public static void main(String[] args) {
+		int i = 5;
+		i++; // i = i+1; 와 같다
+		System.out.println(i);// i 값을 출력한다.
+		
+		i = 5;// i값을 비교하기 위해 값을 다시 5로 설정한다.
+		++i;
+		System.out.println(i);
+
+	}//end - main
+
+}// end -class Operate02
